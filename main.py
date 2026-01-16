eb920b03040cfe60c763eefee4f53647d1c61d79
hottrip <78646579+hottrip227@users.noreply.github.com>
2026-01-16 13:54:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -66,8 +66,10 @@ async def play(ctx, *, search):
         'format': 'bestaudio/best',
         'noplaylist': True,
         'quiet': True,
+        'no_warnings': True,
+        'default_search': 'ytsearch',
+        'source_address': '0.0.0.0',
         'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-        'referer': 'https://www.google.com/',
         }
 
     try:
